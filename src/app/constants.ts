d0a9ab337654368ac0a2e33003b249ace09b9c68
Anchu Thomas <59996876+anchuthomas@users.noreply.github.com>
2026-01-20 19:22:49 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a3ec46a4ed1054ba5a75793203d13c81-791822683.ap-south-1.elb.amazonaws.com',
 };
